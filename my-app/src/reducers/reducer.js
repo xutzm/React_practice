cbf3d957981b10772b6b56fff230f38c86b5ff62
xutzm <xutzm@icloud.com>
2021-09-15 19:03:09 +0300

    REDUX oop class STORE

--- my-app/src/reducers/reducer.js
+++ my-app/src/reducers/reducer.js
@@ -1,5 +1,11 @@
 let state = 0;
 
+const incrementAction = {type:'increment',amount:4};
+const decrementAction = {type:'decrement',amount:2};
+
+
+
+
 function UpdateState(state,action){
     switch(action.type){
         case 'increment':
@@ -11,8 +17,30 @@ function UpdateState(state,action){
     }
 }
 
-const incrementAction = {type:'increment',amount:4};
-const decrementAction = {type:'decrement',amount:2};
+
+
+class Store {
+    constructor(UpdateState,state){
+        this._updateState = UpdateState,
+        this._state=state
+    }
+    getState(){
+        return this._state
+    }
+    update(action){
+        this._state = this._updateState(this._state,action)
+    }
+}
+
+const store = new Store(UpdateState,state);
+
+console.log(store);
+
+store.update(incrementAction);
+
+console.log(store);
+console.log(store.getState());
+
 
 state = UpdateState(state,incrementAction);
 
