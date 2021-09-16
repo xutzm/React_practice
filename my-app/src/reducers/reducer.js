849c05c7108d12d8ac50fc04161763bdf71db1ad
xutzm <xutzm@icloud.com>
2021-09-16 17:37:57 +0300

    subscribe and unsubsribe

--- my-app/src/reducers/reducer.js
+++ my-app/src/reducers/reducer.js
@@ -1,11 +1,13 @@
 let state = 0;
 
+
+//actions
 const incrementAction = {type:'increment',amount:4};
 const decrementAction = {type:'decrement',amount:2};
 
 
 
-
+//reducer
 function UpdateState(state,action){
     switch(action.type){
         case 'increment':
@@ -21,29 +23,44 @@ function UpdateState(state,action){
 
 class Store {
     constructor(UpdateState,state){
-        this._updateState = UpdateState,
-        this._state=state
+        this._updateState = UpdateState;
+        this._state=state;
+        this._callbacks = [];
     }
-    getState(){
+    get state(){
         return this._state
     }
     update(action){
-        this._state = this._updateState(this._state,action)
+        this._state = this._updateState(this._state,action);
+        this._callbacks.forEach(callback => callback());
+    }
+    subscribe(callback){
+        this._callbacks.push(callback);
+        return () => this._callbacks=this._callbacks.filter(cb=>cb!==callback);
     }
 }
 
 const store = new Store(UpdateState,state);
 
-console.log(store);
+// console.log(store);
 
+
+
+const unsubscribe = store.subscribe(()=>console.log('subscribe1 = '+ store.state));
+store.subscribe(()=>console.log('subscribe2 = '+ store.state));
 store.update(incrementAction);
+unsubscribe();
+store.update(decrementAction);
+store.update(decrementAction);
 
-console.log(store);
-console.log(store.getState());
 
 
-state = UpdateState(state,incrementAction);
 
-state = UpdateState(state,decrementAction);
 
-console.log(state);
+
+//OLD
+// state = UpdateState(state,incrementAction);
+
+// state = UpdateState(state,decrementAction);
+
+// console.log(state);
